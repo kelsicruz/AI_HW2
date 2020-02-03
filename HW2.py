380cb5cf1c4b8430cbdae6eddff6345a9ec88d2d
argiesemann <gieseman21@up.edu>
2020-02-02 19:29:18 -0800

    update

--- HW2.py
+++ HW2.py
@@ -9,228 +9,256 @@ from Move import Move
 from GameState import *
 from AIPlayerUtils import *
 
-
+#global vars
+bestFood = None
 ##
 #AIPlayer
 #Description: The responsbility of this class is to interact with the game by
-#deciding a valid move based on a given game state. This class has methods that
-#will be implemented by students in Dr. Nuxoll's AI course.
+#deciding a valid move based on a given game state.  This class has methods
+#that
+#will be implemented by students in Dr.  Nuxoll's AI course.
 #
 #Variables:
 #	playerId - The id of the player.
 ##
 class AIPlayer(Player):
+    def __init__(self, inputPlayerId):
+        super(AIPlayer, self).__init__(inputPlayerId, "HW2")
+        self.resetPlayerData()
+        
+    def resetPlayerData(self):
+        self.myTunnel = None
+        self.myHill = None
+        self.avgDistToFoodPoint = None
 
-	#__init__
-	#Description: Creates a new Player
-	#
-	#Parameters:
-	#	inputPlayerId - The id to give the new player (int)
-	#	cpy			  - whether the player is a copy (when playing itself)
-	##
-	def __init__(self, inputPlayerId):
-		super(AIPlayer,self).__init__(inputPlayerId, "HW2")
-	
-	##
-	#getPlacement
-	#
-	#Description: called during setup phase for each Construction that
-	#	must be placed by the player.  These items are: 1 Anthill on
-	#	the player's side; 1 tunnel on player's side; 9 grass on the
-	#	player's side; and 2 food on the enemy's side.
-	#
-	#Parameters:
-	#	construction - the Construction to be placed.
-	#	currentState - the state of the game at this point in time.
-	#
-	#Return: The coordinates of where the construction is to be placed
-	##
-	def getPlacement(self, currentState):
-		numToPlace = 0
-		#implemented by students to return their next move
-		if currentState.phase == SETUP_PHASE_1:	   #stuff on my side
-			numToPlace = 11
-			moves = []
-			for i in range(0, numToPlace):
-				move = None
-				while move == None:
-					#Choose any x location
-					x = random.randint(0, 9)
-					#Choose any y location on your side of the board
-					y = random.randint(0, 3)
-					#Set the move if this space is empty
-					if currentState.board[x][y].constr == None and (x, y) not in moves:
-						move = (x, y)
-						#Just need to make the space non-empty. So I threw whatever I felt like in there.
-						currentState.board[x][y].constr == True
-				moves.append(move)
-			return moves
-		elif currentState.phase == SETUP_PHASE_2:	#stuff on foe's side
-			numToPlace = 2
-			moves = []
-			for i in range(0, numToPlace):
-				move = None
-				while move == None:
-					#Choose any x location
-					x = random.randint(0, 9)
-					#Choose any y location on enemy side of the board
-					y = random.randint(6, 9)
-					#Set the move if this space is empty
-					if currentState.board[x][y].constr == None and (x, y) not in moves:
-						move = (x, y)
-						#Just need to make the space non-empty. So I threw whatever I felt like in there.
-						currentState.board[x][y].constr == True
-				moves.append(move)
-			return moves
-		else:
-			return [(0, 0)]
-	
-	##
-	#getMove
-	#Description: Gets the next move from the Player.
-	#
-	#Parameters:
-	#	currentState - The state of the current game waiting for the player's move (GameState)
-	#
-	#Return: The Move to be made
-	##
-	def getMove(self, currentState):
-		moves = listAllLegalMoves(currentState)
-		selectedMove = moves[random.randint(0,len(moves) - 1)];
+    def getPlacement(self, currentState):
+    
+        me = currentState.whoseTurn
+        
+        if currentState.phase == SETUP_PHASE_1:
+            #Hill, Tunnel, Grass
+            self.resetPlayerData()
+            self.myNest = (2,1)
+            self.myTunnel = (7,1)
+            return [(2,1), (7, 1), 
+                    (0,3), (1,3), (2,3), (3,3), \
+                    (4,3), (5,3), (6,3), \
+                    (5,2), (6,2)]
+        elif currentState.phase == SETUP_PHASE_2:
+            moves = []
+            for y in range(6, 10):
+                for x in range(0,10):
+                    if currentState.board[x][y].constr == None and len(moves) < 2:
+                        moves.append((x,y))
+            return moves
+            
+        else:            
+            return None  #should never happen
+    
+    ##
+    #getMove
+    #Description: Gets the next move from the Player.
+    #
+    #Parameters:
+    #	currentState - The state of the current game waiting for the player's move
+    #	(GameState)
+    #
+    #Return: The Move to be made
+    ##
+    def getMove(self, currentState):
+        me = currentState.whoseTurn
+        workerAnts = getAntList(currentState, me, (WORKER,))
+        if (me == PLAYER_ONE):
+            enemy = PLAYER_TWO
+        else :
+            enemy = PLAYER_ONE
+        
+        if (self.myTunnel == None):
+            self.myTunnel = getConstrList(currentState, me, (TUNNEL,))[0].coords
+            
+        if (self.myHill == None):
+            self.myHill = getConstrList(currentState, me, (ANTHILL,))[0].coords
+        
+        if (bestFood == None):
+            assignBestFood(currentState, self.myTunnel, self.myHill)
+        #if (self.avgDistToFoodPoint == None and self.bestFood != None):
+        #	for worker in workerAnts:
+        #		print("this loop ran once!\n")
+        #		foodToTunnelDist = stepsToReach(currentState, bestFood[0].coords,
+        #		bestFood[1])
 
-		#don't do a build move if there are already 3+ ants
-		numAnts = len(currentState.inventories[currentState.whoseTurn].ants)
-		while (selectedMove.moveType == BUILD and numAnts >= 3):
-			selectedMove = moves[random.randint(0,len(moves) - 1)];
-			
-		return selectedMove
-	
-	##
-	#getAttack
-	#Description: Gets the attack to be made from the Player
-	#
-	#Parameters:
-	#	currentState - A clone of the current state (GameState)
-	#	attackingAnt - The ant currently making the attack (Ant)
-	#	enemyLocation - The Locations of the Enemies that can be attacked (Location[])
-	##
-	def getAttack(self, currentState, attackingAnt, enemyLocations):
-		#Attack a random enemy.
-		return enemyLocations[random.randint(0, len(enemyLocations) - 1)]
+        
 
-	##
-	#registerWin
-	#
-	# This agent doens't learn
-	#
-	def registerWin(self, hasWon):
-		#method templaste, not implemented
-		pass
+        selectedMove = getMove(currentState)
+            
+        return selectedMove
+    
+    ##
+    #getAttack
+    #Description: Gets the attack to be made from the Player
+    #
+    #Parameters:
+    #	currentState - A clone of the current state (GameState)
+    #	attackingAnt - The ant currently making the attack (Ant)
+    #	enemyLocation - The Locations of the Enemies that can be attacked
+    #	(Location[])
+    ##
+    def getAttack(self, currentState, attackingAnt, enemyLocations):
+        return enemyLocations[0]
+
+    ##
+    #registerWin
+    #
+    # This agent doens't learn
+    #
+    def registerWin(self, hasWon):
+        #method templaste, not implemented
+        pass
 
 def heuristicStepsToGoal(currentState):
-	#test value
-	return 3
-	
-	
-	#returns a heuristic guess of how many moves it will take the agent to win the game starting from the given state
-#divide steps to goal into steps to each type of win
+    #test value
+    return 999999
+        
+        
+        #returns a heuristic guess of how many moves it will take the agent to
+        #win the game starting from the given state
+    #divide steps to goal into steps to each type of win
 def stepsToFoodGoal(currentState):
-	pass
-	#numWorkers
-	
-	#foodScore
-	
-	#avgStepsToFoodPoint
+    #get the board
+    # fastClone(currentState)
+        
+    #get numWorkers
+    workerList = getAntList(currentState, me, (WORKER,))
+    numWorkers = len(workerList)
 
-#added comment
-
-def stepsToFoodPoint(currentState, workerAnt):
-	pass
+    #foodScore
+    myInv = getCurrPlayerInventory(currentState)
+    numFood = myInv.foodCount
+    foodLoc = self.myFood.coords
+    anthillLoc = myInv.getAnthill().coords
+        
+    toFood = 0
+    toHill = 0
+    
+    #avgStepsToFoodPoint
+    for ant in workerList:
+        toFood += stepsToFoodPoint(currentState, ant, foodLoc)
+        toHill += stepsToReach(currentState, foodLoc, anthillLoc)
+        cycle = toFood + toHill
+        # save in external variable? like ant1, ant2, etc?
+        
+        
+    
+def stepsToFoodPoint(currentState, workerAnt, foodLocation):
+    steps = stepsToReach(currentState, workerAnt.coords, foodLocation)
+    return steps
 
 def stepsToQueenGoal(currentState):
-	pass
-	
+    pass
+    
 def stepsToAntHillGoal(currentState):
-	pass
-	
+    pass
+    
 def getMove(currentState):
-	moves = listAllLegalMoves(currentState)
-	
-	moveNodes = []
-	
-	for move in moves:
-		nextState = getNextState(currentState, move)
-		stateUtility = heuristicStepsToGoal(nextState)
-		node = MoveNode(move, nextState)
-		node.setUtility(stateUtility)
-		moveNodes.append(node)
-		
-	bestMoveFromNodeList = bestMove(moveNodes).move
-			
-	return bestMoveFromNodeList
+    moves = listAllLegalMoves(currentState)
+    
+    moveNodes = []
+    
+    for move in moves:
+        nextState = getNextState(currentState, move)
+        stateUtility = heuristicStepsToGoal(nextState)
+        node = MoveNode(move, nextState)
+        node.setUtility(stateUtility)
+        moveNodes.append(node)
+        
+    bestMoveFromNodeList = bestMove(moveNodes).move
+            
+    return bestMoveFromNodeList
 
 def bestMove(moveNodes):
-	bestNodeUtility = 999999999
-	bestNode = None
-	for moveNode in moveNodes:
-		if (moveNode.utility < bestNodeUtility):
-			bestNode = moveNode
-			bestNodeUtility = moveNode.utility
-	
-	return bestNode
-			
+    bestNodeUtility = 999999999
+    bestNode = None
+    for moveNode in moveNodes:
+        if (moveNode.utility < bestNodeUtility):
+            bestNode = moveNode
+            bestNodeUtility = moveNode.utility
+    
+    return bestNode
+
+def assignBestFood(currentState, myTunnel, myHill):
+    
+    foods = getConstrList(currentState, None, (FOOD,))
+    bestTunnelDist = 50
+    bestHillDist = 50
+    bestTunnelFood = None
+    bestHillFood = None
+            
+    for food in foods:
+        dist = stepsToReach(currentState, myTunnel, food.coords)
+        if (dist < bestTunnelDist) :
+            bestTunnelFood = food
+            bestTunnelDist = dist
+        dist = stepsToReach(currentState, myHill, food.coords)
+        if (dist < bestHillDist) :
+            bestHillFood = food
+            bestHillDist = dist
+            
+    if (bestHillDist < bestTunnelDist):
+        bestFood = (bestHillFood, myHill)
+    else :
+        bestFood = (bestTunnelFood, myTunnel)
 
 class MoveNode():
-	
-	def __init__(self, move, state):
-		self.move = move
-		self.state = state
-		self.depth = 1
-		self.utility = None
-		self.parent = None
-		
-	def setUtility(self, newUtility):
-		self.utility = newUtility + self.depth
-		
+    
+    def __init__(self, move, state):
+        self.move = move
+        self.state = state
+        self.depth = 1
+        self.utility = None
+        self.parent = None
+        
+    def setUtility(self, newUtility):
+        self.utility = newUtility + self.depth
+        
 ##
 #TEST CODE FOLLOWS
 ##
 print("Test code is being run")
 
 testState = GameState.getBasicState()
-		
+        
 #getMove() test
 move = getMove(testState)
 if (move == None):
-	print("Error in getMove(). Null move returned.\n")
-		
-		
-		
+    print("Error in getMove(). Null move returned.\n")
+        
+        
+        
 #bestMove() test
 possibleMoves = listAllLegalMoves(testState)
 
 moveNodes = []
 for move in possibleMoves:
-	nextState = getNextState(testState, move)
-	stateUtility = heuristicStepsToGoal(nextState)
-	node = MoveNode(move, nextState)
-	node.setUtility(stateUtility)
-	moveNodes.append(node)
+    nextState = getNextState(testState, move)
+    stateUtility = heuristicStepsToGoal(nextState)
+    node = MoveNode(move, nextState)
+    node.setUtility(stateUtility)
+    moveNodes.append(node)
 
 bestNode = bestMove(moveNodes)
 
 if (bestNode == None):
-	print("Error in bestMove(). Null node returned.\n")
+    print("Error in bestMove(). Null node returned.\n")
 else:
-	if (bestNode.utility == None):
-		print("Error in bestMove(). Utility was not set.\n")
-		
+    if (bestNode.utility == None):
+        print("Error in bestMove(). Utility was not set.\n")
+        
 print("Test code has been run")
 
 
 
 
-		
-			
-		
-			
+        
+            
+        
+            
